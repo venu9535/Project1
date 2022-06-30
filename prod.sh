57ac79e4edf47d85b20f2cdaaaa674d1c744cec5
venu9535 <85005548+venu9535@users.noreply.github.com>
2022-06-30 10:23:06 +0530

    upadting the product

--- prod.sh
+++ prod.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "enter the first nu"
-read no
-echo "enter the second nu"
-read no1
-prod=`expr $no \* $no1`
+num1=10
+num2=20
+prod=`expr $num1 \* $num2`
 echo "the prod is $prod"
